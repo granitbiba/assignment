b1f1eccaa1826203c8b8ef518eb4b2673cf49e8e
granitbiba <granitbiba@gmail.com>
2025-03-17 21:59:59 +0100

    updated the host,port

--- assignment_1/app.py
+++ assignment_1/app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello Solaborate, this is the web app from the first assignment"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=5000)
