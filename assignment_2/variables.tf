e5b76424e0e70ba93b0a40ea6941c50cbb62cc82
granitbiba <granitbiba@gmail.com>
2025-03-17 22:45:35 +0100

    added variables file

---
+++ assignment_2/variables.tf
@@ -0,0 +1,19 @@
+variable "tenant_id" {
+  type        = string
+  description = "tenant_id"
+}
+
+variable "client_id" {
+  type        = string
+  description = "client_id"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "client_secret"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "subscription_id"
+}
